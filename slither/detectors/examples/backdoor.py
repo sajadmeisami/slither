c15e8c4df81da2652827d7c7d9ff8ba15bb6224f
sajad <sajadmeysami@yahoo.com>
2023-09-08 11:58:11 -0500

    could find ecrecover and its containing function

--- slither/detectors/examples/backdoor.py
+++ slither/detectors/examples/backdoor.py
@@ -1,5 +1,97 @@
 from typing import List
 
+from abc import ABC
+
+
+from inspect import currentframe, getframeinfo
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+from typing import Optional, List, Dict, Set, Callable, Tuple, TYPE_CHECKING, Union
+from slither.core.cfg.node import Node, NodeType
+from slither.core.declarations.contract import Contract
+from slither.core.compilation_unit import SlitherCompilationUnit
+from slither.core.declarations.structure import Structure
+from slither.core.declarations.structure_contract import StructureContract
+from slither.core.declarations.solidity_variables import SolidityVariable, SolidityFunction
+from slither.core.variables.variable import Variable
+from slither.core.variables.state_variable import StateVariable
+from slither.core.variables.local_variable import LocalVariable
+from slither.core.variables.structure_variable import StructureVariable
+from slither.core.expressions.identifier import Identifier
+from slither.core.expressions.literal import Literal
+from slither.core.declarations.function_contract import FunctionContract
+from slither.core.expressions.expression import Expression
+
+from slither.core.expressions.tuple_expression import TupleExpression
+from slither.core.expressions.literal import Literal
+from slither.core.expressions.call_expression import CallExpression
+from slither.core.expressions.type_conversion import TypeConversion
+from slither.core.expressions.assignment_operation import AssignmentOperation
+from slither.core.expressions.conditional_expression import ConditionalExpression
+from slither.core.expressions.binary_operation import BinaryOperation, BinaryOperationType
+from slither.core.expressions.member_access import MemberAccess
+from slither.core.expressions.index_access import IndexAccess
+from slither.core.solidity_types.mapping_type import MappingType, Type
+from slither.core.solidity_types.user_defined_type import UserDefinedType
+from slither.core.solidity_types.elementary_type import ElementaryType
+from slither.slithir.variables.temporary import TemporaryVariable
+from slither.utils.function import get_function_id
+import slither.analyses.data_dependency.data_dependency as data_dependency
+
+from slither.detectors.abstract_detector import (
+    AbstractDetector,
+    DetectorClassification,
+    SupportedOutput,
+    Output,
+)
+
+
+from typing import Optional, List, Dict, Callable, Tuple, TYPE_CHECKING, Union
+from slither.core.cfg.node import NodeType
+from slither.core.declarations.contract import Contract
+
+from slither.core.declarations.structure import Structure
+from slither.core.declarations.structure_contract import StructureContract
+from slither.core.declarations.modifier import Modifier
+from slither.core.variables.variable import Variable
+from slither.core.variables.state_variable import StateVariable
+from slither.core.variables.local_variable import LocalVariable
+from slither.core.variables.structure_variable import StructureVariable
+from slither.core.expressions.identifier import Identifier
+from slither.core.expressions.literal import Literal
+from slither.core.declarations.function_contract import FunctionContract
+
+from slither.core.expressions.call_expression import CallExpression
+from slither.core.expressions.type_conversion import TypeConversion
+from slither.core.expressions.assignment_operation import AssignmentOperation
+from slither.core.expressions.binary_operation import BinaryOperation
+from slither.core.expressions.member_access import MemberAccess
+from slither.core.expressions.index_access import IndexAccess
+from slither.core.solidity_types.mapping_type import MappingType
+from slither.core.solidity_types.user_defined_type import UserDefinedType
+from slither.core.solidity_types.elementary_type import ElementaryType
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+from typing import Optional, List, Dict, Callable, Tuple, TYPE_CHECKING, Union
+from slither.core.cfg.node import NodeType
+from slither.core.declarations.contract import Contract
+from slither.core.declarations.structure import Structure
+from slither.core.variables.variable import Variable
+from slither.core.variables.state_variable import StateVariable
+from slither.core.variables.local_variable import LocalVariable
+from slither.core.variables.structure_variable import StructureVariable
+from slither.core.expressions.identifier import Identifier
+from slither.core.expressions.literal import Literal
+from slither.core.declarations.function_contract import FunctionContract
+
+from slither.core.expressions.call_expression import CallExpression
+from slither.core.expressions.type_conversion import TypeConversion
+from slither.core.expressions.assignment_operation import AssignmentOperation
+from slither.core.expressions.binary_operation import BinaryOperation
+from slither.core.expressions.member_access import MemberAccess
+from slither.core.expressions.index_access import IndexAccess
+from slither.core.solidity_types.mapping_type import MappingType
+from slither.core.solidity_types.user_defined_type import UserDefinedType
+from slither.core.solidity_types.elementary_type import ElementaryType
+
 from slither.detectors.abstract_detector import (
     AbstractDetector,
     DetectorClassification,
