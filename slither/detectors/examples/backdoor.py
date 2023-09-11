3f9622eb320251e0bf0f599dd2ad1a54456e45a7
Hugo DABADIE <hugo.dabadie@grenoble-inp.org>
2023-09-11 19:03:41 +0200

    successors and callers

--- slither/detectors/examples/backdoor.py
+++ slither/detectors/examples/backdoor.py
@@ -2,7 +2,6 @@ from typing import List
 
 from abc import ABC
 
-
 from inspect import currentframe, getframeinfo
 from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
 from typing import Optional, List, Dict, Set, Callable, Tuple, TYPE_CHECKING, Union
@@ -43,7 +42,6 @@ from slither.detectors.abstract_detector import (
     SupportedOutput,
     Output,
 )
-
 
 from typing import Optional, List, Dict, Callable, Tuple, TYPE_CHECKING, Union
 from slither.core.cfg.node import NodeType
@@ -153,29 +151,64 @@ class Backdoor(AbstractDetector):
         for contract in self.compilation_unit.contracts_derived:
             for function in contract.functions:
                 for node in function.nodes:
-                    #print(str(node.expression))
+                    # print(str(node.expression))
                     if "ecrecover" in str(node.expression):
-                        ecrecover_usage = not ecrecover_usage
-                        ecrecover_count = ecrecover_count + 1
-                        A = node.function
-                        print(A.name)
-                        #print(A.internal_calls)
+                        ecrecover_usage = True
+                        ecrecover_count += 1
+                        ecrecover_func = node.function
+                        print(ecrecover_func.name)
+
+                        signature_validitycheck = True  # TODO: check “ecrecover” returning value (source) dependency on an address from one of the function parameters (PO: parameter owner) (sink) and not zero in conditional statement (If, require, bool)
+
+                        # Get all the functions that call ecrecover_func
+                        funcs_that_calls_ecrecover_func = set(ecrecover_func.all_reachable_from_functions)
+
+                        for func in funcs_that_calls_ecrecover_func:
+                            parameters = func.parameters
+                            # TODO: check if conditional statement
+                            # TODO: check if ecrecover return == address
+
+                        print([el.name for el in funcs_that_calls_ecrecover_func])
+                        # Get all the functions called by ecrecover_func excluding solidity ones
+                        funcs_called_by_ecrecover_func = set(ecrecover_func.all_internal_calls()) - set(ecrecover_func.all_solidity_calls())
+                        #solidity_func = ecrecover_func.all_solidity_calls()[0]
+                        #print(solidity_func.full_name)
+                        #print([el.to_json() for el in solidity_func.references])
+                        #print([el.name for el in funcs_called_by_ecrecover_func])
+
+                        fun = ecrecover_func.all_solidity_calls()[0]
+                        #print(fun.name)
+                        #print(fun.source_mapping)
+
+                        funcs_to_inspect = funcs_that_calls_ecrecover_func.union(funcs_called_by_ecrecover_func)
+                        funcs_inspected = set()
+
+                        while funcs_to_inspect != set():
+                            inspecting_func = funcs_to_inspect.pop()
+                            funcs_inspected.add(inspecting_func)
+                            #print(inspecting_func.name)
+                            #print([(el.node.function.name, [par.name for par in el.node.function.parameters]) for el in inspecting_func.])
+                            #for arg in inspecting_func.parameters:
 
 
-                        for ir in A.all_slithir_operations():
-                            if isinstance(ir, Call):
-                                #print(ir)
-                                if isinstance(ir, SolidityCall):
-                                    print(ir.function)
-                                    #if "ecrecover" in ir.function.name:
-                                        #print(ir.function)
-                                elif isinstance(ir, InternalCall):
-                                    A_child=ir.function.nodes
-                                    #print(A_child)
 
+                            # Add the functions called by the function analyzed
+                            # (functions that calls it are already added by "all_reachable_from_functions")
+                            #funcs_called_by_inspecting_func = set(inspecting_func.all_internal_calls()) - set(inspecting_func.all_solidity_calls()) - funcs_inspected
+                            #funcs_to_inspect.update(funcs_called_by_inspecting_func)
 
+                        # for ir in A.all_slithir_operations():
+                        #     if isinstance(ir, Call):
+                        #         # print(ir)
+                        #         if isinstance(ir, SolidityCall):
+                        #             print(ir.function)
+                        #             # if "ecrecover" in ir.function.name:
+                        #             # print(ir.function)
+                        #         elif isinstance(ir, InternalCall):
+                        #             A_child = ir.function.nodes
+                        #             # print(A_child)
 
-                        info: DETECTOR_INFO = ["ecrecover found in function: ", A.name, "\n"]
+                        info: DETECTOR_INFO = ["ecrecover found in function: ", ecrecover_func.name, "\n"]
 
                         # Add the result in result
                         res = self.generate_result(info)
@@ -183,5 +216,3 @@ class Backdoor(AbstractDetector):
                         results.append(res)
 
         return results
-
-
